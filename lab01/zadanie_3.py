bea41caebbf3d0fa3ca4c035f5e87807dbb6f8aa
Wiktorpluzek <75083416+Wiktorpluzek@users.noreply.github.com>
2022-03-15 20:40:36 +0100

    Update zadanie_3.py

--- lab01/zadanie_3.py
+++ lab01/zadanie_3.py
@@ -1,4 +1,20 @@
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
 
-data = pandas.read_csv('miasta.csv')
-print(data.to_string())
+miasta = pd.read_csv('./miasta.csv')
+
+data = {"Rok": [2010], "Gdansk": [460], "Poznan": [555], "Szczecin": [405]}
+toAdd = pd.DataFrame(data)
+toAdd.to_csv('./miasta.csv', mode='a', index=False, header=False)
+
+
+cols_list = ["Rok", "Gdansk", "Poznan", "Szczecin"]
+miasta = pd.read_csv('./miasta.csv', usecols=cols_list)
+
+plt.plot(miasta["Rok"], miasta["Gdansk"], label="Gdańsk")
+plt.plot(miasta["Rok"], miasta["Szczecin"], label="Szczecin")
+plt.plot(miasta["Rok"], miasta["Poznan"], label="Poznań")
+plt.xlabel("Lata")
+plt.ylabel("Liczba ludności [tys]")
+plt.legend()
+plt.show()
