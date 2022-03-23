e990e4e19dc3007621f64b7328989e4bc3ed7750
Wiktorpluzek <75083416+Wiktorpluzek@users.noreply.github.com>
2022-03-23 13:45:29 +0100

    Add files via upload

--- lab3/zad3.py
+++ lab3/zad3.py
@@ -4,7 +4,7 @@ import time
 import math
 
 labirynt = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 2, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
             [1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1],
             [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
             [1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1],
@@ -13,7 +13,7 @@ labirynt = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
             [1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1],
             [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 3, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
 
@@ -31,15 +31,30 @@ def fitness_func(solution, solution_idx):
     y=1
     for i in solution:
         if i == 0:
-            if labirynt[x][y-1]==1:
+            if labirynt[y-1][x]==1:
                 pass
-            if labirynt[x][y-1]==3:
-                return 0
+            elif labirynt[y-1][x]==0:
+                y-=1
         if i == 1:
-
+            if labirynt[y][x+1]==1:
+                pass
+            elif labirynt[y][x+1]==0:
+                x+=1
+            elif labirynt[y][x+1]==2:
+                return 0
         if i == 2:
-
+            if labirynt[y+1][x]==1:
+                pass
+            elif labirynt[y+1][x]==0:
+                y+=1
+            elif labirynt[y+1][x]==2:
+                return 0
         if i == 3:
+            if labirynt[y][x-1]==1:
+                pass
+            elif labirynt[y][x-1]==0:
+                x-=1
+    fitness = -((11 - x) + (11 - y))
     return fitness
 
 
@@ -47,15 +62,15 @@ fitness_function = fitness_func
 
 # ile chromsomów w populacji
 # ile genow ma chromosom
-sol_per_pop = 300
+sol_per_pop = 30
 num_genes = len(S)
 
 # ile wylaniamy rodzicow do "rozmanazania" (okolo 50% populacji)
 # ile pokolen
 # ilu rodzicow zachowac (kilka procent)
-num_parents_mating = 30
+num_parents_mating = 10
 num_generations = 5000
-keep_parents = 10
+keep_parents = 2
 
 # jaki typ selekcji rodzicow?
 # sss = steady, rws=roulette, rank = rankingowa, tournament = turniejowa
@@ -80,7 +95,8 @@ ga_instance = pygad.GA(gene_space=gene_space,
                        keep_parents=keep_parents,
                        crossover_type=crossover_type,
                        mutation_type=mutation_type,
-                       mutation_percent_genes=mutation_percent_genes)
+                       mutation_percent_genes=mutation_percent_genes,
+                       stop_criteria=["reach_0"])
 
 # uruchomienie algorytmu
 ga_instance.run()
@@ -89,6 +105,18 @@ ga_instance.run()
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
 generations = ga_instance.generations_completed
 print("Parameters of the best solution : {solution}".format(solution=solution))
+directions = []
+for i in solution:
+    if i==0:
+        directions.append("Gora")
+    if i==1:
+        directions.append("Prawo")
+    if i==2:
+        directions.append("Dol")
+    if i==3:
+        directions.append("Lewo")
+
+print(directions)
 print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 
 # tutaj dodatkowo wyswietlamy sume wskazana przez jedynki
